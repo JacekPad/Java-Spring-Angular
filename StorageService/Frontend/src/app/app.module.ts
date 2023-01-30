561bb3c72313d9552be2a5b9f295e827694d54a0
jacek <jacekpadyjasek@gmail.com>
2023-01-30 11:05:24 +0100

    navbar/sidebar/footer frontend

--- StorageService/Frontend/src/app/app.module.ts
+++ StorageService/Frontend/src/app/app.module.ts
@@ -3,19 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { StorageModule } from './storage/storage.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { TopbarComponent } from './topbar/topbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
     NavbarComponent,
     NotFoundComponent,
+    TopbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
